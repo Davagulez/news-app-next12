dd8935ebd79d59242c762498082f1273e54539bb
Davagulez <lezcadav@gmail.com>
2023-12-04 16:37:02 -0300

    Arreglando un poco los resultados del fetch en index.js

--- pages/index.js
+++ pages/index.js
@@ -6,38 +6,37 @@ import PageLayout from '../components/PageLayout'
 import { useEffect, useState } from 'react'
 
 export default function Home() {
-  [articles, setArticles] = useState([])
+  const [articles, setArticles] = useState([])
 
   useEffect(() => {
-    fetch('https://newsapi.org/v2/everything?q=tesla&from=2023-11-04&sortBy=publishedAt&apiKey=ad64c8bce4954149826c2db88c6dddec')
+    fetch('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=ad64c8bce4954149826c2db88c6dddec')
     .then(res => res.json())
     .then(response => {
-      const [articles] = response
-      setArticles(articles)
+      const articlesFetched = response.articles;
+      setArticles(articlesFetched)
     })
-  })
+  },[])
 
+  console.log('articulos recibidos de la api', articles)
   return (
     <PageLayout title='NewsApp - Home'>
       <div className={styles.container}>
-      {articles.length === 0 && <p>Loading ...</p>}
-      {articles.length > 0 && articles.map(art, index => {
-        <article key={index}>
-          <img 
-            alt={`Image for the article ${art.title}`} 
-            src={art.urlToImage}
-          />
-          <h2> 
-              {art.title}
-          </h2>
-          <p>
-            {art.description}
-          </p>
-        </article>
-      })}
+        {articles.length === 0 && <p>Loading ...</p>}
+        {articles.length > 0 && articles.map((art, index) => ( 
+          <article key={index}>
+            <img 
+              alt={`Image for the article ${art.title}`} 
+              src={art.urlToImage === null ? '/404notfound.jpg' : art.urlToImage}
+              className={styles.image}
+            />
+            <h2>{art.title}</h2>
+            <p>{art.description}</p>
+          </article>
+        ))}
         <Link href='/about'> ir a About</Link>     
       </div>
     </PageLayout>
   )
 }
+
 
